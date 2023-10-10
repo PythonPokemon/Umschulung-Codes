d9289f5556493caf98ceabeca5d844d338c282e5
PythonPokemon <Jakob.D.Baron@Outlook.de>
2023-10-10 14:27:40 +0200

    update

--- Durchschnittswert_S_309_1.py
+++ Durchschnittswert_S_309_1.py
@@ -1,7 +1,7 @@
 x = 0
-x += float(input("Wert1: "))
-x += float(input("Wert2: "))
-x += float(input("Wert3: "))
-x += float(input("Wert4: "))
-x += float(input("Wert5: "))
-print("Ergebnis:", x/5)
+x += float(input("Wert1: ")) # einhabe gleitkjomma + kummuliert mit vorherieger eingabe
+x += float(input("Wert2: ")) # ''
+x += float(input("Wert3: ")) # ''
+x += float(input("Wert4: ")) # ''
+x += float(input("Wert5: ")) # ''
+print("Ergebnis:", x/5) # ergebnis geteilt durch die anzahl der Zahleneingabe
