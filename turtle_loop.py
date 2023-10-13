3792368ffcb0edd3352932625aad96e5bf6613d5
PythonPokemon <Jakob.D.Baron@Outlook.de>
2023-10-13 09:57:01 +0200

    update

--- turtle_loop.py
+++ turtle_loop.py
@@ -5,11 +5,24 @@ s = turtle.getscreen()
 t = turtle.Turtle()
 
 t.pen(pensize=3)
-
 t.color("purple")
 
-colors = ["green", ]
+colors = ["green", "blue", "red", "yellow", "orange"]  # Hier kannst du weitere Farben hinzufügen
 
 for i in range(100, 0, -10):
     t.begin_fill()
-    t.color
+    t.color(random.choice(colors))  # Zufällige Farbauswahl aus der Liste "colors"
+    
+    for _ in range(4):
+        t.forward(i)
+        t.right(90)
+    
+    t.end_fill()
+    t.penup()
+    t.forward(10)
+    t.left(90)
+    t.forward(10)
+    t.right(90)
+    t.pendown()
+
+turtle.done()
